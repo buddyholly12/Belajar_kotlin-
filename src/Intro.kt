d7af2593e53d72f6bd11b147f75bb937552ed36a
buddyholly12 <tekweb2018@gmail.com>
2020-08-16 19:10:47 +0700

    belajar tutorial kotlin

---
+++ src/Intro.kt
@@ -0,0 +1,8 @@
+fun main(){
+    // Intro Print hello world
+        //println("hello World")
+// Intro input output / i/o
+    //print("Enter a number: ")
+    //val reader = readLine()
+    //print("ini angka input :$reader")
+}
